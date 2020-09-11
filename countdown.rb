218a4dadee3bf79ea0805201b7d37f804020517f
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-11 13:32:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(number)
   while number > 0
-    num -= 1
+    number -= 1
     puts "#{number} SECOND(S)!"
   end
   "HAPPY NEW YEAR!"
@@ -11,7 +11,7 @@ end
 
 def countdown_to_sleep(number)
   while number > 0
-    num -= 1
+    number -= 1
     puts "#{number} SECOND(S)!"
     sleep(1)
   end
