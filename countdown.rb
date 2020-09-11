18e3c6fc6b1682e65c0097f6233577fef50330cc
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-11 13:30:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while num > 0
+    num -= 1
+    puts "#{number} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
 end
+
+
+def countdown_to_sleep(number)
